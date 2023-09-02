359833bb17e82816e9df80126eec3b4a2978866d
miksin <contact@miksin.art>
2023-09-02 17:30:16 +0900

    prototype

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <h2>Miksin.art</h2>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<style scoped>
+  .background-light {
+    background-color: #FAFAFA;
+  }
+  .main {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 412px;
+    min-height: calc(100vh - 280px);
+    padding-top: 140px;
+    padding-bottom: 140px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+<template>
+  <div class="background-light">
+    <div class="main background-light">
+      <slot />
+    </div>
+  </div>
+</template>
