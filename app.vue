fb525f197ad9ef19028781f2b88eedc40a20ea2b
miksin <contact@miksin.art>
2023-09-03 19:04:47 +0900

    title

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup>
 useHeadSafe({
+  title: 'Miksin.art',
   link: [
     { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
