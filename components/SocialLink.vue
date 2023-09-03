931dd0a243534660f07cb82a7320d45ca8f033c6
miksin <contact@miksin.art>
2023-09-03 17:00:09 +0900

    title & favicon & social links

---
+++ components/SocialLink.vue
@@ -0,0 +1,34 @@
+<style scoped>
+.link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #424242;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  transition: scale 251ms ease-in-out;
+}
+
+.link:hover {
+  scale: 1.2;
+}
+
+.icon {
+  width: 64%;
+  height: 64%;
+}
+</style>
+
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  href: string;
+}>()
+</script>
+
+<template>
+  <a class="link" :href="href" target="_blank" rel="noopener noreferrer">
+    <img class="icon" :src="src" />
+  </a>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <style scoped>
   .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     margin: auto;
+    gap: 1.5rem;
+  }
+
+  .social-links {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+  }
+
+  .title {
+    font-size: 3rem;
+    letter-spacing: -0.06rem;
   }
 </style>
 
 <template>
   <div class="main">
-    <h1>Miksin.art</h1>
+    <h3 class="title">Miksin.art</h3>
+    <div class="social-links">
+      <SocialLink src="/img/x.svg" href="https://twitter.com/miksin_" />
+      <SocialLink src="/img/instagram.svg" href="https://www.instagram.com/miksin_" />
+      <SocialLink src="/img/pixiv.png" href="https://www.pixiv.net/users/3774481" />
+      <SocialLink src="/img/yt.svg" href="https://www.youtube.com/channel/UCmYleBoaTtNtPVZkaUwFxOw" />
+    </div>
   </div>
 </template>
