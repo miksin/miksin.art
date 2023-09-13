5e61f2ba5cd9955d1ede94ea9c443dda0b569210
miksin <contact@miksin.art>
2023-09-13 16:57:24 +0900

    fadeIn

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,14 @@
 <style scoped>
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      filter: blur(1rem);
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
   .background-light {
     background-color: #FAFAFA;
   }
@@ -10,12 +20,13 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    animation: fadeIn 451ms ease;
   }
 </style>
 
 <template>
   <div class="background-light">
-    <div class="main background-light">
+    <div class="main">
       <slot />
     </div>
   </div>
